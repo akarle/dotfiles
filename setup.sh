34086462f61dd8856a07a1d3210e28feb7f069c0
Alexander Karle <akarle@umass.edu>
2017-12-27 14:47:42 -0500

    First attempt at a setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+# After cloning dotfiles into ~/git/dotfiles
+# this file sets up vimrc and tmux.conf
+
+# Set up vimrc
+VIMHOME="~/.vim"
+if [ ! -d $VIMHOME ]; then
+    echo "Making ~/.vim"
+    mkdir $VIMHOME
+fi
+
+ln -s ~/git/dotfiles/vimrc ~/.vimrc
+ln -s ~/git/dotfiles/theme.vim ~/.vim/theme.vim
+ln -s ~/git/dotfiles/plugins.vim ~/.vim/plugins.vim
+ln -s ~/git/dotfiles/pluginsettings.vim ~/.vim/pluginsettings.vim
+ln -s ~/git/dotfiles/keys.vim ~/.vim/keys.vim
