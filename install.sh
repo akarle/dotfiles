c719595d85adc55a254315d7c7f96ffe7f5f675d
Alex Karle <alex@karle.co>
2019-08-25 23:30:25 -0400

    bin: small updates to install.sh
    I am now thinking that the best way to do this is to git clone the
    dotfiles repo oneself, then let install setup vim/dotfiler/symlinks.
    
    This is easier, since I can never remember the url for the raw blob for
    curl :)

--- install.sh
+++ install.sh
@@ -7,15 +7,14 @@ if ! [ -x "$(command -v git)" ]; then
   exit 1
 fi
 
+mkdir -p $HOME/.config
 mkdir -p $HOME/dev/proj
 
 echo "Installing dotvim   -> $HOME/.vim"
-git clone --recursive https://github.com/akarle/dotvim $HOME/.vim
+git clone --recursive https://github.com/akarle/dotvim $HOME/.vim || exit 1
 echo "Installing dotfiler -> $HOME/dev/proj/dotfiler"
-git clone https://github.com/akarle/dotfiler $HOME/dev/proj/dotfiler
-echo "Installing dotfiles -> $HOME/dev/dotfiles"
-git clone https://github.com/akarle/dotfiles $HOME/dev/dotfiles
+git clone https://github.com/akarle/dotfiler $HOME/dev/proj/dotfiler || exit 1
 
 echo "Using dotfiler to link bash (bare minimum)"
 PATH=$HOME/dev/proj/dotfiler:$PATH
-dotfiler.pl -d $HOME/dev/dotfiles -l bash
+dotfiler.pl -d $HOME/dev/dotfiles -l bash tmux
