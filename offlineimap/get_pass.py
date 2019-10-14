beed39afb3d3dfe5a89e3a4dc668ae2e1459dd23
Alex Karle <alex@karle.co>
2019-10-14 00:42:09 -0400

    offlineimap: add minimal config
    Heavily inspired by the offlineimap Arch Wiki page as well as the
    minimal example that ships with the package.
    
    I'm toying around with local Maildir's for my personal email. As I
    subscribe to more mailing lists, it really is a better way to improve
    MUA speed / snappiness. I also like the idea of owning the hardcopies of
    my emails should I ever need them.

---
+++ offlineimap/get_pass.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# Small python script to retrieve encrypted gpg email password
+# Heavily inspired by Arch Wiki's offlineimap docs
+from subprocess import check_output
+
+def get_pass():
+    return check_output("gpg -dq ~/.config/mutt/accounts/karleco.gpg", shell=True).strip("\n")
+
+# If called via CLI, output the password to STDOUT
+# Useful for things like GIT_ASKPASS
+if __name__ == "__main__":
+    print(get_pass())
