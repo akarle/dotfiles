8a18ad8088e61b139feea6bcb893cd3a1710582c
Alex Karle <alex@alexkarle.com>
2022-02-23 01:20:02 -0500

    nvim: Add persistent undo files
    So pleased to see that I don't need to jump through the usual hoops
    of creating ~/.vim/undo and putting a .gitkeep file there and
    gitignoring everything else... nvim just creates a ~/.local/share
    directory by itself and handles it all!

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,9 @@
 -- init.lua ; my nvim config
 vim.cmd [[colorscheme acme]]
 
+-- Persistent undo files in ~/.local/share/nvim/undo
+vim.opt.undofile = true
+
 -- Switch between windows with Ctrl-h/j/k/l
 vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true })
