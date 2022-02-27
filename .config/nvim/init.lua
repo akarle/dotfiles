7a1ef860bf214ece9b14879a6e99474106e79f9e
Alex Karle <alex@alexkarle.com>
2022-02-27 12:01:32 -0500

    nvim: Source local.lua, if it exists
    Again, this is nice for site-local configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,3 +21,6 @@ vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true })
+
+-- Lastly, source a local config, if it exists
+vim.cmd [[runtime! local.lua]]
