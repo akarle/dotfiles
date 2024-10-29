1e39e2782fc70dcb7f8e4bd065f666d0e7daede4
Alex Karle <alex@alexkarle.com>
2024-10-28 21:14:35 -0400

    nvim: 2 spaces for kotlin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -24,6 +24,7 @@ vim.cmd [[autocmd FileType typescriptreact setlocal et sw=2 sts=2]]
 vim.cmd [[autocmd FileType scheme          setlocal et sw=2 sts=2]]
 vim.cmd [[autocmd FileType html            setlocal et sw=2 sts=2]]
 vim.cmd [[autocmd FileType css             setlocal et sw=2 sts=2]]
+vim.cmd [[autocmd FileType kotlin          setlocal et sw=2 sts=2]]
 vim.cmd [[autocmd BufRead,BufNewFile *.jsx set filetype=javascript]]
 
 
